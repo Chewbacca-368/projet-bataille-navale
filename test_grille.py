755ffbfcb30e079c3b06e21256a40b01cb174acd
Quentin <quentin.amiot18@gmail.com>
2025-11-16 16:14:43 +0100

    ✅ Tests de la classe Grille

--- test_grille.py
+++ test_grille.py
@@ -1,6 +1,17 @@
 from grille import Grille
 
 
-def test_constructeur():
-    g = Grille()
+def test_init():
+    g = Grille(5, 8)
     assert isinstance(g, Grille)
+    assert g.nb_lignes == 5
+    assert g.nb_colonnes == 8
+    assert len(g.matrice) == 5 * 8
+    assert all(case == g.vide for case in g.matrice)
+
+
+def test_tirer():
+    g = Grille(5, 8)
+    g.tirer(4, 3)
+    impact = g._index(4, 3)
+    assert g.matrice[impact] == g.touche
