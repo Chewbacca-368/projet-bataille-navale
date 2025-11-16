32c6dba31157acfd9438b9408ccadebad3b783b1
Quentin <quentin.amiot18@gmail.com>
2025-11-16 17:50:47 +0100

    🛥️ Ajout de la classe Bateau et des tests associés

---
+++ test_bateau.py
@@ -0,0 +1,14 @@
+from bateau import Bateau
+
+
+def test_init():
+    b1 = Bateau(2, 3)
+    assert isinstance(b1, Bateau)
+    assert b1.ligne == 2
+    assert b1.colonne == 3
+    assert b1.longueur == 1
+    assert not b1.vertical
+
+    b2 = Bateau(2, 2, longueur=4, vertical=True)
+    assert b2.longueur == 4
+    assert b2.vertical
